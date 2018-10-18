b0746826e12e37022f65ee5d03fae21052cfb63c
RafaJow <rafaelfagundes64@gmail.com>
2018-10-18 15:46:02 -0300

    Só mudei Tabuleiro

--- README.md
+++ README.md
@@ -17,3 +17,13 @@ __Elementos do jogo__
 - **Elemento tipo ingrediente:** (a cada rodada o jogador pode conquistar mais um ingrediente para a sua pizza. Verificar no tabuleiro os ingredientes dispon√≠veis.
 - **Elemento tipo sorte ou azar:** caso o jogador pare o seu pe√£o na parte indicando sorte ou azar, o mesmo vira uma ficha a qual cont√©m um pr√™mio ou uma puni√ß√£o.
 - **Funcionamento do jogo:** todos os jogadores iniciam em uma posi√ß√£o do tabuleiro com um pe√£o. A cada rodada, os jogadores jogam o dado e seguem adiante n casas no tabuleiro, de acordo com o n√∫mero indicado pelo dado e recebe o ingrediente, a puni√ß√£o ou o pr√™mio (retirando a ficha sorte ou azar).
+
+
+__TABELA / TABULEIRO__
+A tabela È uma lista circular duplamente encadeada para que o jogador consiga se movimentar por ela sem a possibilidade de encontrar o final da lista, dando assim uma imers„o de um jogo infinito. Cada jogador possuir· um tabuleiro como seu atributo, o atributo servir· para o usu·rio localizar sua posiÁ„o no tabuleiro. A pizza È uma lista de ingredientes que È definido pelo sabor de pizza escolhido pelo usu·rio. o jogo possui eventos espalhados pelo tabuleiros, entre eles existe o sorte ou azar que s„o 20 eventos diferentes.
+
+Tabuleiro (Lista circular duplamente encadeada)
+String Evento
+Tabuleiro anterior
+Tabuleiro prÛximo
+
--- src/projeto/Tabuleiro.java
+++ src/projeto/Tabuleiro.java
@@ -1,6 +1,137 @@
 package projeto;
 
 public class Tabuleiro {
-
+//Lista dupla circular
+	
+	private Tabela atual, ultimo, primeiro;
+	
+	
+	public Tabuleiro() {
+		Tabela atual = null;
+		Tabela ultimo = null;
+		Tabela primeiro = null;
+	}
+	
+	public boolean estaVazia() {
+		return primeiro == null;
+	}
+	
+	public int comprimento() {
+		int comp =0;
+		atual = primeiro;
+		while(atual != null) {
+			comp++;
+			atual = atual.getProximo();
+		}
+		return comp;
+	}
+	
+	//Checar se precisa de parametro pra criar Tabela, caso precise alterar metodo inserePrimeiro() abaixo
+	public void inserePrimeiro() {
+		Tabela novo = new Tabela();
 		
+		if(estaVazia()) {
+			novo.setProximo(novo);
+			novo.setAnterior(novo);
+			primeiro = novo;
+			ultimo = novo;
+			atual = novo;
+		}else {
+			novo.setProximo(primeiro);
+			novo.setAnterior(ultimo);
+			primeiro.setAnterior(novo);
+			primeiro = novo;
+			ultimo.setProximo(primeiro);
+		}
+	}
+	
+	//mesma coisa do metodo inserePrimeiro(), checar se precisa de parametro pra criar tabela
+	public void insereUltimo() {
+		Tabela novo = new Tabela();
+		
+		if(estaVazia()) {
+			novo.setProximo(novo);
+			novo.setAnterior(novo);
+			primeiro = novo;
+			ultimo = novo;
+			atual = novo;
+		}else {
+			ultimo.setProximo(novo);
+			novo.setAnterior(ultimo);
+			novo.setProximo(primeiro);
+			primeiro.setAnterior(novo);
+			ultimo = novo;
+		}
+	}
+	
+	
+	//MOVE PARA A POSIÇÃO INDICADA
+	public void moveParaPosicao(int pos) {
+		atual = primeiro;
+		for (int i = 0; i < pos; i++) {
+			atual = atual.getProximo();
+		}
+	}
+	
+	/* INSERE UM NOVO OBJETO NA POSICAO INDICADA,
+	 * mesma coisa dos metodos inserePrimeiro() e insereUltimo(), verificar se é necessario
+	 * passar algum parametro para criar Tabela...
+	 * */
+	public void insereNaPosicao(int pos) {
+		Tabela novo = new Tabela();
+		moveParaPosicao(pos);
+		novo.setProximo(atual.getProximo());
+		novo.setAnterior(atual);
+		atual.setProximo(novo);
+	}
+	
+	//Retorna o elemento na posição indicada
+	public Tabela elementoNaPosicao(int pos) {
+		moveParaPosicao(pos);
+		return atual;
+	}
+	
+	//Retorna a posicao de um elemento na lista, se não existir retorna um numero negativo(palavras do luciano)
+	public int buscaElemento(String n) {
+		//No caso "String n" seria algo para comparar, o nome no caso, mas dai tem q ver isso
+		
+		int cont =0;
+		atual = primeiro;
+		
+		while(atual != null && atual.getCasa().equalsIgnoreCase(n)) {
+			atual = atual.getProximo();
+			cont++;
+		}
+		if(atual != null) {
+			return cont;
+		}
+		return -1;
+	}
+	
+	//Remove primeira posicao
+	public void removePrimeiro() {
+		if(estaVazia() == false) {
+			primeiro = primeiro.getProximo();
+			primeiro.setAnterior(ultimo);
+			ultimo.setProximo(primeiro);
+		}else {
+			System.out.println("Ta vazio, nao ha nada pra remover");
+		}
+	}
+	
+	//Remove ultima posicao
+	public void removeUltimo() {
+		if(estaVazia() == false) {
+			int pos;
+			pos=comprimento()-1;
+			moveParaPosicao(pos);
+			atual.setProximo(primeiro);
+			primeiro.setAnterior(atual);
+			ultimo = atual;
+		}else {
+			System.out.println("Ta vazio, nao ha nada pra remover");
+		}
+	}
+	
 }
+
