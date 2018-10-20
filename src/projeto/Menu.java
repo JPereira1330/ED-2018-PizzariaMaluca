14fcc16394894c1c8ed0fa556de33abfdb5b6000
José claudio Pereira <jpereira1330@gmail.com>
2018-10-20 18:53:15 -0300

    Menu

--- src/projeto/Menu.java
+++ src/projeto/Menu.java
@@ -38,17 +38,51 @@ public class Menu {
 
 	private static void iniciarJogo() {
 	
+		int quantia = 0;
+		boolean concluido = false;
 		Scanner scan = new Scanner(System.in);
 		
 		// Limpando a tela
 		clear(4);
 		
 		// Menu de seleção
-		while(true) {
+		while(!concluido) {
 			System.out.println(" [ * ] Insira a quantia de jogadores [ 2 ~ 5 ] ~> ");
-			try { int quantia = scan.nextInt(); } 
+
+			try { quantia = scan.nextInt(); } 
 			catch (Exception e) { System.out.println(); }
+			
+			if(quantia <= 5 && quantia >= 2)
+				concluido = true;
 		}
+		
+		// Resetando Scan
+		scan = new Scanner(System.in);
+		
+		for(int i = 0; i < quantia; i++) {
+		
+			// Pritando as pizzas
+			System.out.println(" NOME DA PIZZA  | INGREDIENTES NECESSARIOS");
+			System.out.println(" Calabresa		[ Azeitona, Calabresa, Ervilha,   Brocolis,  Ovo 		]");
+			System.out.println(" Marguerita		[ Queijo,   Milho,     Tomate,    Calabresa, Brocolis 	]");
+			System.out.println(" Romana			[ Queijo,   Milho,     Cebola,    Presunto,  Ervilha 	] ");
+			System.out.println(" Toscana		[ Presunto, Tomate,    Calabresa, Cebola,    Azeitona	]");
+			System.out.println(" Portuguesa 	[ Queijo,   Milho,     Ovo,       Azeitona,  Presunto	]");
+			System.out.println(" Vegetariana	[ Ovo, 	    Tomate,    Cebola,    Brocolis,  Ervilha 	]");
+			System.out.print  ("\n [ Jogador "+i+" ] -> ");
+			
+			String sabor = scan.nextLine();
+			sabor = sabor.toLowerCase();
+			
+			if(sabor.equals("calabresa") || sabor.equals("Marguerita") || sabor.equals("Romana") || sabor.equals("Toscana") || sabor.equals("Portuguesa") || sabor.equals("Vegetariana")){
+				//Jogador jogador = new Jogador();
+				//jogador.
+			}else {
+				i--;
+				System.out.println(" [ ERRO ] SABOR INEXISTENTE");
+			}
+		}
+		
 		
 	}
 	
--- src/projeto/Teste.java
+++ src/projeto/Teste.java
@@ -8,25 +8,7 @@ public class Teste {
 	
 	public static void testar() {
 		
-		Pizza pizza = Inicializador.montarPizza("Calabresa");
-		System.out.println("1 - Listando a pizza montada");
-		pizza.listar();
-		System.out.println("\n");
-		
-		System.out.println("2 - Removendo OVO..");
-		pizza.rem("Ovo");
-		System.out.println("\n");
-		
-		System.out.println("3 - Listando alteracoes..");
-		pizza.listar();
-		System.out.println("\n");
-		
-		System.out.println("4 - Removendo CALABRESA");
-		pizza.rem("Calabresa");
-		System.out.println("\n");
-		
-		System.out.println("5 - Listando altercoes");
-		pizza.listar();
+		Menu.exec();
 		
 	}
 	
