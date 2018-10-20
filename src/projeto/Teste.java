ad8ff986f8c98edb87f9638b18068ad0481977c9
José claudio Pereira <jpereira1330@gmail.com>
2018-10-20 15:39:08 -0300

    Adicionando classe para realizar teste

---
+++ src/projeto/Teste.java
@@ -0,0 +1,33 @@
+package projeto;
+
+public class Teste {
+
+	/*
+	 * CLASSE CRIADA PARA TESTAR O CODIGO
+	 */
+	
+	public static void testar() {
+		
+		Pizza pizza = Inicializador.montarPizza("Calabresa");
+		System.out.println("1 - Listando a pizza montada");
+		pizza.listar();
+		System.out.println("\n");
+		
+		System.out.println("2 - Removendo OVO..");
+		pizza.rem("Ovo");
+		System.out.println("\n");
+		
+		System.out.println("3 - Listando alteracoes..");
+		pizza.listar();
+		System.out.println("\n");
+		
+		System.out.println("4 - Removendo CALABRESA");
+		pizza.rem("Calabresa");
+		System.out.println("\n");
+		
+		System.out.println("5 - Listando altercoes");
+		pizza.listar();
+		
+	}
+	
+}
--- src/projeto/main.java
+++ src/projeto/main.java
@@ -5,26 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		
 		//SorteAzar listaSorteAzar = Inicializador.gerarSorteAzar();
-		
-		Pizza pizza = Inicializador.montarPizza("Calabresa");
-		System.out.println("1 - Listando a pizza montada");
-		pizza.listar();
-		System.out.println("\n");
-		
-		System.out.println("2 - Removendo OVO..");
-		pizza.rem("Ovo");
-		System.out.println("\n");
-		
-		System.out.println("3 - Listando alteracoes..");
-		pizza.listar();
-		System.out.println("\n");
-		
-		System.out.println("4 - Removendo CALABRESA");
-		pizza.rem("Calabresa");
-		System.out.println("\n");
-		
-		System.out.println("5 - Listando altercoes");
-		pizza.listar();
+		Teste.testar();
 		
 	}
 
